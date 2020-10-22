c54f4b90e3d187a9eafa080d813dafa195da4eed
Md Habibullah Bin Ismail <bahar61119@gmail.com>
2020-10-23 00:35:50 +0100

    ProfileUpdate ui updated

--- src/app/controllers/views/loadAppViews.ts
+++ src/app/controllers/views/loadAppViews.ts
@@ -1,6 +1,17 @@
 import { ViewFileNames } from "../../constants/fileNames";
+import { loadHomeView } from './loadHomeViews';
+import { loadProfileUpdateView } from './loadProfileViews';
 import { loadView } from './loadView';
 
-export function loadAppView() {
-    return loadView(ViewFileNames.APP);
+export function loadAppView(appContentFileName: string, data: {[key: string]: any} = {}) {
+    let appContents: string;
+
+    switch(appContentFileName) {
+        case ViewFileNames.PROFILE_UPDATE:
+            appContents = loadProfileUpdateView(data.user);
+            break;
+        default:
+            appContents = loadHomeView();
+    }
+    return loadView(ViewFileNames.APP, {appContents});
 }
--- src/app/controllers/views/loadIndexViews.ts
+++ src/app/controllers/views/loadIndexViews.ts
@@ -1,14 +1,16 @@
 import { AppConstant } from '../../constants/appConstants';
 import { ViewFileNames } from "../../constants/fileNames";
+import { User } from '../../models/userModel';
 import { loadAppView } from './loadAppViews';
-import { loadProfileUpdateView } from './loadProfileViews';
 
 export function loadIndexView(profileUpdate: boolean = false) {
     const htmlService = HtmlService.createTemplateFromFile(ViewFileNames.INDEX);
+    let data: {[key: string]: any} = {}
     if(profileUpdate) {
-        htmlService.bodyContent = loadProfileUpdateView();
+        data.user = User.of();
+        htmlService.bodyContent = loadAppView(ViewFileNames.PROFILE_UPDATE, data);
     } else {
-        htmlService.bodyContent = loadAppView();
+        htmlService.bodyContent = loadAppView(ViewFileNames.HOME, data);
     }
     return htmlService.evaluate().setTitle(AppConstant.APP_NAME);
 }
