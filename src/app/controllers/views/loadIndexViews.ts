57c78087ff309f36d7d1633d2eb09c8f9d47c7a3
Md Habibullah Bin Ismail <bahar61119@gmail.com>
2020-10-25 01:54:47 +0000

    Navbar Profile Update updated

--- src/app/controllers/views/loadIndexViews.ts
+++ src/app/controllers/views/loadIndexViews.ts
@@ -1,17 +1,14 @@
 import { AppConstant } from '../../constants/appConstants';
 import { ViewFileNames } from "../../constants/fileNames";
-import { User } from '../../models/userModel';
 import { UserDBService } from '../../services/userDBService';
 import { loadAppView } from './loadAppViews';
 
 export function loadIndexView() {
     const htmlService = HtmlService.createTemplateFromFile(ViewFileNames.INDEX);
-    let data: {[key: string]: any} = {}
     if(!UserDBService.doesCurrentUserExist()) {
-        data.user = User.of();
-        htmlService.bodyContent = loadAppView(ViewFileNames.PROFILE_UPDATE, data);
+        htmlService.bodyContent = loadAppView(ViewFileNames.PROFILE_UPDATE);
     } else {
-        htmlService.bodyContent = loadAppView(ViewFileNames.HOME, data);
+        htmlService.bodyContent = loadAppView(ViewFileNames.HOME);
     }
     return htmlService.evaluate().setTitle(AppConstant.APP_NAME);
 }
--- src/app/controllers/views/loadProfileViews.ts
+++ src/app/controllers/views/loadProfileViews.ts
@@ -1,7 +1,12 @@
 import { ViewFileNames } from '../../constants/fileNames';
 import { User } from '../../models/userModel';
+import { UserDBService } from '../../services/userDBService';
 import { loadView } from './loadView';
 
-export function loadProfileUpdateView(user: User = User.of()) {
+export function loadProfileUpdateView() {
+    let user: User = User.of();
+    if(UserDBService.doesCurrentUserExist()) {
+        user = UserDBService.getCurrentUser();
+    }
     return loadView(ViewFileNames.PROFILE_UPDATE, {user});
 }
