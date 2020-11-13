4bf9b65ea1e475469c3ce5d475de46e19c40f1bf
Md Habibullah Bin Ismail <bahar61119@gmail.com>
2020-11-13 00:10:50 +0000

    Supplier Types addedas select to supplier update

--- src/app/controllers/views/loadSupplierViews.ts
+++ src/app/controllers/views/loadSupplierViews.ts
@@ -1,6 +1,8 @@
+import { ApplicationDBKeys } from '../../constants/applicationDBKeys';
 import { ViewFileNames } from "../../constants/fileNames";
 import { Supplier } from '../../models/supplierModel';
 import { ProfileService } from '../../services/profileService';
+import { SettingsService } from '../../services/settingsService';
 import { SupplierService } from '../../services/supplierService';
 import { loadView } from './loadView';
 
@@ -11,8 +13,10 @@ function loadSupplierListView() {
 
 function loadAddSupplierView() {
   ProfileService.validateProfile();
+  let supplierTypes = SettingsService.getList(ApplicationDBKeys.SUPPLIER_TYPES);
   let data = {
     supplier: Supplier.of(),
+    supplierTypes,
     isEdit: false
   }
   return loadView(ViewFileNames.UPDATE_SUPPLIER, data);
@@ -21,8 +25,10 @@ function loadAddSupplierView() {
 function loadEditSupplierView(supplierId: string) {
   ProfileService.validateProfile();
   let supplier = SupplierService.getSupplier(String(supplierId));
+  let supplierTypes = SettingsService.getList(ApplicationDBKeys.SUPPLIER_TYPES);
   let data = {
     supplier,
+    supplierTypes,
     isEdit: true
   }
   return loadView(ViewFileNames.UPDATE_SUPPLIER, data);
