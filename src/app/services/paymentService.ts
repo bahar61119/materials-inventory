10b16b02b0047a290f62c866244d83e7bf3f00fc
Md Habibullah Bin Ismail <bahar61119@gmail.com>
2020-11-25 22:54:06 +0000

    Payments List and Payments Update updated

--- src/app/services/paymentService.ts
+++ src/app/services/paymentService.ts
@@ -1,24 +1,29 @@
 import { SheetConstants } from '../constants/sheetConstants';
+import { Invoice } from '../models/invoiceModel';
 import { Payment } from '../models/paymentModel';
 import { Supplier } from '../models/supplierModel';
 import { Utils } from '../utils/utils';
 import { EntityService } from './entityService';
+import { InvoiceService } from './invoiceService';
 import { SupplierService } from './supplierService';
 
 export class PaymentService extends EntityService {
     private static DATE_FORMAT = "yyyy-MM-dd";
     static getPaymentList() {
         let paymentList: Array<Payment> = PaymentService.getEntityList(SheetConstants.PAYMENTS_SHEET_NAME, Payment.name);
-        let supplierList: Array<Supplier> = SupplierService.getSupplierList();
+        let supplierList: Array<Supplier> = PaymentService.getEntityList(SheetConstants.SUPPLIER_SHEET_NAME, Supplier.name);
         let supplierMap = Utils.arrayToObject(supplierList, "supplierId");
-        console.log(supplierMap);
+        let invoiceList: Array<Invoice> = PaymentService.getEntityList(SheetConstants.INVOICES_SHEET_NAME, Invoice.name);
+        let invoiceMap = Utils.arrayToObject(invoiceList, "invoiceId");
         return paymentList.map((payment: Payment) => {
             payment.paymentDueDate = PaymentService.convertDateString(payment.paymentDueDate, PaymentService.DATE_FORMAT);
             payment.paymentDate = PaymentService.convertDateString(payment.paymentDate, PaymentService.DATE_FORMAT);
             let supplier: Supplier = supplierMap[payment.paymentSupplier];
+            let invoice: Invoice = invoiceMap[payment.paymentInvoice];
             return {
                 ...payment,
-                paymentSupplierName: supplier? supplier.supplierName: ''
+                paymentSupplierName: supplier? supplier.supplierName: '',
+                paymentInvoiceName: invoice? invoice.invoiceName: ''
             }
         });
     }
