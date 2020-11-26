42bf38c12f886687fda8664ef486a1c254878337
Md Habibullah Bin Ismail <bahar61119@gmail.com>
2020-11-26 01:42:29 +0000

    Entry List updated

--- src/app/controllers/views/loadEntryViews.ts
+++ src/app/controllers/views/loadEntryViews.ts
@@ -1,12 +1,27 @@
+import { ApplicationDBKeys } from '../../constants/applicationDBKeys';
 import { ViewFileNames } from '../../constants/fileNames';
 import { Entry } from '../../models/entryModel';
 import { EntryService } from '../../services/entryService';
+import { InvoiceService } from '../../services/invoiceService';
+import { ItemsService } from '../../services/itemsService';
 import { ProfileService } from '../../services/profileService';
+import { SettingsService } from '../../services/settingsService';
+import { SupplierService } from '../../services/supplierService';
 import { loadView } from './loadView';
 
 export function loadEntryListView() {
     ProfileService.validateProfile();
-    return loadView(ViewFileNames.ENTRY_LIST);
+    let suppliers = SupplierService.getSupplierList();
+    let invoices = InvoiceService.getInvoiceList();
+    let items = ItemsService.getItemList();
+    let productStatusList = SettingsService.getList(ApplicationDBKeys.PRODUCT_STATUS);
+    let data = {
+        suppliers,
+        invoices,
+        items,
+        productStatusList
+    }
+    return loadView(ViewFileNames.ENTRY_LIST, data);
 }
 
 export function loadAddEntryView() {
--- src/app/services/entryService.ts
+++ src/app/services/entryService.ts
@@ -1,15 +1,34 @@
 import { SheetConstants } from '../constants/sheetConstants';
 import { Entry } from '../models/entryModel';
+import { Invoice } from '../models/invoiceModel';
+import { Item } from '../models/itemModel';
+import { Supplier } from '../models/supplierModel';
+import { Utils } from '../utils/utils';
 import { EntityService } from './entityService';
+import { PaymentService } from './paymentService';
 
 export class EntryService extends EntityService {
     private static DATE_FORMAT = "yyyy-MM-dd";
     static getEntryList() {
         let entryList: Array<Entry> = EntryService.getEntityList(SheetConstants.ENTRIES_SHEET_NAME, Entry.name);
+        let supplierList: Array<Supplier> = PaymentService.getEntityList(SheetConstants.SUPPLIER_SHEET_NAME, Supplier.name);
+        let supplierMap = Utils.arrayToObject(supplierList, "supplierId");
+        let invoiceList: Array<Invoice> = PaymentService.getEntityList(SheetConstants.INVOICES_SHEET_NAME, Invoice.name);
+        let invoiceMap = Utils.arrayToObject(invoiceList, "invoiceId");
+        let itemList: Array<Item> = PaymentService.getEntityList(SheetConstants.ITEMS_SHEET_NAME, Item.name);
+        let itemMap = Utils.arrayToObject(itemList, "itemId");
         return entryList.map((entry: Entry) => {
             entry.entryExpectedDeliveryDate = EntryService.convertDateString(entry.entryExpectedDeliveryDate, EntryService.DATE_FORMAT);
             entry.entryDeliveryDate = EntryService.convertDateString(entry.entryDeliveryDate, EntryService.DATE_FORMAT);
-            return entry;
+            let supplier: Supplier = supplierMap[entry.entrySupplier];
+            let invoice: Invoice = invoiceMap[entry.entryInvoice];
+            let item: Item = itemMap[entry.entryItem];
+            return {
+                ...entry,
+                entrySupplierName: supplier? supplier.supplierName: '',
+                entryInvoiceName: invoice? invoice.invoiceName: '',
+                entryItemName: item? item.itemName: '',
+            }
         });
     }
 
