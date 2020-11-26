df1652c82b95d88a48fbf0c2e0a8dca92a6c2731
Md Habibullah Bin Ismail <bahar61119@gmail.com>
2020-11-26 01:42:29 +0000

    Entry List updated

--- src/app/controllers/apis/fileApis.ts
+++ src/app/controllers/apis/fileApis.ts
@@ -2,5 +2,5 @@ import { File } from '../../models/fileModel';
 import { FileService } from '../../services/fileService';
 
 export function uploadFile(file: any) {
-    return FileService.uploadFile(file);
+    return FileService.uploadFile(File.from(file));
 }
