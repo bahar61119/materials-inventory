9998630deaee8e5319cf8713219b01a55a7a5877
Md Habibullah Bin Ismail <bahar61119@gmail.com>
2020-11-27 02:34:15 +0000

    Invoice Payments added

--- src/app/services/invoiceService.ts
+++ src/app/services/invoiceService.ts
@@ -2,6 +2,7 @@ import { FolderNames } from '../constants/driveConstants';
 import { SheetConstants } from '../constants/sheetConstants';
 import { Drive } from '../db/drive';
 import { Invoice } from '../models/invoiceModel';
+import { Payment } from '../models/paymentModel';
 import { Supplier } from '../models/supplierModel';
 import { Utils } from '../utils/utils';
 import { EntityService } from './entityService';
@@ -11,6 +12,7 @@ export class InvoiceService extends EntityService{
     static getInvoiceList() {
         let invoiceList: Array<Invoice> = InvoiceService.getEntityList(SheetConstants.INVOICES_SHEET_NAME, Invoice.name);
         let supplierList: Array<Supplier> = InvoiceService.getEntityList(SheetConstants.SUPPLIER_SHEET_NAME, Supplier.name);
+        let paymentList: Array<Payment> = InvoiceService.getEntityList(SheetConstants.PAYMENTS_SHEET_NAME, Payment.name);
         let supplierMap = Utils.arrayToObject(supplierList, "supplierId"); 
         return invoiceList.map((invoice: Invoice) => {
             invoice.invoiceIssueDate = InvoiceService.convertDateString(invoice.invoiceIssueDate, InvoiceService.DATE_FORMAT);
@@ -21,10 +23,25 @@ export class InvoiceService extends EntityService{
                 let file = DriveApp.getFileById(invoice.invoiceFile);
                 invoiceFileName = file.getName();
             }
+
+            let invoicePaidPayment = InvoiceService.getInvoiceTotalPayment(invoice.invoiceId, paymentList);
+            let balance = invoice.invoiceAmount - invoicePaidPayment.totalPaidAmount;
+            let payable = 0;
+            let receivable = 0;
+            if (balance > 0) {
+                payable = Math.abs(balance);
+            } else {
+                receivable = Math.abs(balance);
+            }
+            
             return {
                 ...invoice,
                 invoiceSupplierName: supplier? supplier.supplierName: '',
-                invoiceFileName
+                invoiceFileName,
+                invoiceTotalPaidPayments: invoicePaidPayment.totalPaidPayments,
+                invoiceTotalPaidAmount: invoicePaidPayment.totalPaidAmount,
+                invoicePayable: payable,
+                invoiceReceivable: receivable
             }
         });
     }
@@ -65,5 +82,20 @@ export class InvoiceService extends EntityService{
             Drive.removeFile(currentInvoice.invoiceFile);
         }
         return invoiceId
+    }
+
+    private static getInvoiceTotalPayment(invoiceId: string, paymentList: Array<Payment>) {
+        let totalPaidPayments = 0;
+        let totalPaidAmount = 0.0;
+        paymentList.forEach(payment => {
+            if (payment.paymentInvoice === invoiceId && payment.paymentStatus === DefaultValues.PaymentStatus.PAID) {
+                totalPaidPayments++;
+                totalPaidAmount = totalPaidAmount + payment.paymentAmount;
+            }
+        });
+        return {
+            totalPaidPayments,
+            totalPaidAmount
+        }
     }
 }
