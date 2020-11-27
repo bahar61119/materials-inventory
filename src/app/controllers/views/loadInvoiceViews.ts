dfb11c8fe196ba5600c6ec08ab6863d804c5ba40
Md Habibullah Bin Ismail <bahar61119@gmail.com>
2020-11-27 02:12:33 +0000

    Currencies removed from invoices

--- src/app/controllers/views/loadInvoiceViews.ts
+++ src/app/controllers/views/loadInvoiceViews.ts
@@ -1,9 +1,7 @@
-import { ApplicationDBKeys } from '../../constants/applicationDBKeys';
 import { ViewFileNames } from '../../constants/fileNames';
 import { Invoice } from '../../models/invoiceModel';
 import { InvoiceService } from '../../services/invoiceService';
 import { ProfileService } from '../../services/profileService';
-import { SettingsService } from '../../services/settingsService';
 import { SupplierService } from '../../services/supplierService';
 import { loadView } from './loadView';
 
@@ -19,11 +17,9 @@ function loadInvoiceListView() {
 function loadAddInvoiceView() {
     ProfileService.validateProfile();
     let suppliers = SupplierService.getSupplierList();
-    let currencies = SettingsService.getList(ApplicationDBKeys.CURRENCIES);
     let data = {
         invoice: Invoice.of(),
         suppliers,
-        currencies,
         isEdit: false
     }
     return loadView(ViewFileNames.UPDATE_INVOICE, data);
@@ -33,11 +29,9 @@ function loadEditInvoiceView(invoiceId: string) {
     ProfileService.validateProfile();
     let invoice = InvoiceService.getInvoice(String(invoiceId));
     let suppliers = SupplierService.getSupplierList();
-    let currencies = SettingsService.getList(ApplicationDBKeys.CURRENCIES);
     let data = {
         invoice,
         suppliers,
-        currencies,
         isEdit: true
     }
     return loadView(ViewFileNames.UPDATE_INVOICE, data);
