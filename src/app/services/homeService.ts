48d91e29538a94c3705ca6762fc17aa5bff1eeed
Md Habibullah Bin Ismail <bahar61119@gmail.com>
2020-11-28 22:45:32 +0000

    Home Dashboard updated

--- src/app/services/homeService.ts
+++ src/app/services/homeService.ts
@@ -5,15 +5,21 @@ import { Invoice } from '../models/invoiceModel';
 import { Payment } from '../models/paymentModel';
 import { Supplier } from '../models/supplierModel';
 import { EntryService } from './entryService';
+import { InvoiceService } from './invoiceService';
 import { PaymentService } from './paymentService';
 import { SupplierService } from './supplierService';
 
 export class HomeService {
-    static getDashboard(homeFilter: HomeFilter) {
-        let invoiceList: Array<Invoice> = SupplierService.getEntityList(SheetConstants.INVOICES_SHEET_NAME, Invoice.name);
-        let paymentList: Array<Payment> = PaymentService.getEntityList(SheetConstants.PAYMENTS_SHEET_NAME, Payment.name);
-        let entryList: Array<Entry> = EntryService.getEntityList(SheetConstants.ENTRIES_SHEET_NAME, Entry.name);
-        let supplierList: Array<Supplier> = SupplierService.getEntityList(SheetConstants.SUPPLIER_SHEET_NAME, Supplier.name);
+    static getDashboard(filter: HomeFilter) {
+        console.log(filter);
+        let invoiceList: Array<Invoice> = HomeService.getFilteredInvoiceList(filter);
+        console.log("invoiceList: " + invoiceList.length);
+        let paymentList: Array<Payment> = HomeService.getFilteredPaymentList(filter);
+        console.log("paymentList: " + paymentList.length);
+        let entryList: Array<Entry> = HomeService.getFilteredEntryList(filter);
+        console.log("entryList: " + entryList.length);
+        let supplierList: Array<Supplier> = HomeService.getFilteredSupplierList(filter);
+        console.log("supplierList: " + supplierList.length);
         let supplierListWithInvoiceData = SupplierService.getSupplierWithInvoicesData(supplierList, invoiceList, paymentList);
         let amountByPaymentStatus = HomeService.getAmountByPaymentStatus(paymentList);
         let entriesByDeliveryStatus = HomeService.getEntriesByDeliveryStatus(entryList);
@@ -22,6 +28,39 @@ export class HomeService {
             amountByPaymentStatus,
             entriesByDeliveryStatus,
             ...supplierSummary
+        }
+    }
+
+    private static getFilteredPaymentList(filter: HomeFilter) {
+        let paymentList: Array<Payment> = PaymentService.getEntityList(SheetConstants.PAYMENTS_SHEET_NAME, Payment.name);
+        if(filter.supplier && filter.supplier !== 'all') {
+            paymentList = paymentList.filter(payment => payment.paymentSupplier === filter.supplier);
+        }
+        return paymentList;
+    }
+
+    private static getFilteredInvoiceList(filter: HomeFilter) {
+        let invoiceList: Array<Invoice> = InvoiceService.getEntityList(SheetConstants.INVOICES_SHEET_NAME, Invoice.name);
+        if(filter.supplier && filter.supplier !== 'all') {
+            invoiceList = invoiceList.filter(invoice => invoice.invoiceSupplier === filter.supplier);
+        }
+        return invoiceList;
+    }
+
+    private static getFilteredEntryList(filter: HomeFilter) {
+        let entryList: Array<Entry> = EntryService.getEntityList(SheetConstants.ENTRIES_SHEET_NAME, Entry.name);
+        if(filter.supplier && filter.supplier !== 'all') {
+            entryList = entryList.filter(entry => entry.entrySupplier === filter.supplier);
+        }
+        return entryList;
+    }
+
+    private static getFilteredSupplierList(filter: HomeFilter): Array<Supplier> {
+        if(filter.supplier && filter.supplier !== 'all') {
+            let supplier: Supplier = SupplierService.getEntity(filter.supplier, SheetConstants.SUPPLIER_SHEET_NAME, Supplier.name);
+            return supplier? [supplier]: [];
+        } else {
+            return SupplierService.getEntityList(SheetConstants.SUPPLIER_SHEET_NAME, Supplier.name);
         }
     }
 
